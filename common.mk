960953a4bcafbe073487901c9127c6ff58b7cea2
Keertesh <keertesh.k98@gmail.com>
2022-03-05 03:44:24 +0530

    avicii-common: Build libqdMetaData.system
    *needed by libwfdmmsrc_system.so
    
    *F linker  : CANNOT LINK EXECUTABLE "/system_ext/bin/wfdservice": library "libqdMetaData.system.so" not found: needed by /system_ext/lib/libwfdmmsrc_system.so in namespace (default)
    
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- common.mk
+++ common.mk
@@ -241,6 +241,7 @@ PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
     libdisplayconfig.qti.vendor \
     libqdMetaData \
+    libqdMetaData.system \
     libsdmcore \
     libsdmutils \
     libtinyxml \
